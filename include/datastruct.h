b2ea86d3b7f66a504a8bd5d907c4a7dfa232391b
Gui <guilherme.albarrans@gmail.com>
2018-08-02 22:54:47 -0300

    Functional Stage for Numbers

--- include/datastruct.h
+++ include/datastruct.h
@@ -4,13 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct List{
+#include <string.h>
+
+typedef struct node{
     void* data;
+    int size;
+    struct node* next;
+}List;
 
-};
+List* list_new(void*, int);
+int list_push(List*, void* , int);
+int list_pop(List**);
+int list_clear(List**);
 
-void list_new();
-void list_push(void* , int);
-void list_pop();
+int list_printi(List*);
+int list_prints(List* list);
+
+//void list_add();      // Adiciona item no meio da lista pelo indice
+//void list_find();     // encontra um item da lista pelo valor
+//void list_count();    // conta o numero de registro da lista
+//void list_remove();   // remove um item do meio da lista pelo indice ???
+//void list_get();      // encontra um item da lista pelo indice ???
 
 #endif
--- src/list.c
+++ src/list.c
@@ -1,6 +1,99 @@
 #include "datastruct.h"
 
-void teste()
+List* list_new(void* value, int size)
 {
-    printf("Hello World!\n");
+    // create node
+    List* list = (List*) malloc( sizeof(List) );
+    memset( list, 0, sizeof(List) );
+
+    // value
+    list->data = malloc( size );
+    memset( list->data, 0, size );
+    memcpy(list->data, value, size);
+
+    //size
+    list->size = size;
+
+    return list;
+}
+
+int list_push(List* list, void* value, int size)
+{
+    if(list == NULL) return 0;
+    else
+    {
+        if(list->next == NULL)
+        {
+            list->next = list_new(value, size);
+            return 1;
+        }
+        else
+            return 1+list_push(list->next, value, size);
+    }
+}
+
+int list_pop(List** list)
+{
+    if(list == NULL) return 0;
+    else
+    {
+        List* next = (*list)->next;
+        if(next == NULL)
+        {
+            free((*list)->data);
+            free(*list);
+            (*list) = NULL;
+            return 1;
+        }
+        else
+        {
+            if(next->next == NULL)
+            {
+                free((*list)->next->data);
+                free((*list)->next);
+                (*list)->next = NULL;
+
+                return 2;
+            }
+            else
+                return 1+list_pop(&next);
+        }
+    }
+}
+
+int list_clear(List** list)
+{
+    if((*list) == NULL)return 0;
+    else 
+    {
+        list_pop(list);
+        return 1+list_clear(list);
+    }
+   
+        
+}
+
+int list_printi(List* list)
+{
+    if(list != NULL)
+    {
+        // Atualmente exibe apenas listas com valores inteiros.
+        // A ideia é modificar esta parte para permitir a exibição de qualquer tipo, mas isso ainda precisa ser elaborado  
+        printf(" -> (%d) %d \n", list->size, *(int*)list->data);
+        return 1+list_printi(list->next);
+    }
+    else
+        return 0;
+}
+int list_prints(List* list)
+{
+    if(list != NULL)
+    {
+        // Atualmente exibe apenas listas com valores inteiros.
+        // A ideia é modificar esta parte para permitir a exibição de qualquer tipo, mas isso ainda precisa ser elaborado  
+        printf(" -> (%d) %s \n", list->size, (char*)list->data);
+        return 1+list_prints(list->next);
+    }
+    else
+        return 0;
 }
